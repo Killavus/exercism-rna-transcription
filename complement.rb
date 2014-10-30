e5c2a8288774e64aa152d16a6ff1a65c0354df3f
Marcin Grzywaczewski <killavus@gmail.com>
2014-10-30 23:55:32 +0100

    Refactored to Translator object with Strategy-like translation choosing

--- complement.rb
+++ complement.rb
@@ -1,23 +1,54 @@
 class Complement
   def self.of_dna(dna_strand)
-    dna_strand.chars.map do |nucleotide|
-      case nucleotide
-        when "C" then "G"
-        when "G" then "C"
-        when "T" then "A"
-        when "A" then "U"
-      end
-    end.join
+    NucleotideStrandTranslator.new(DnaToRnaTranslation.new).translate(dna_strand)
   end
 
   def self.of_rna(rna_strand)
-    rna_strand.chars.map do |nucleotide|
-      case nucleotide
-        when "C" then "G"
-        when "G" then "C"
-        when "U" then "A"
-        when "A" then "T"
-      end
-    end.join
+    NucleotideStrandTranslator.new(RnaToDnaTranslation.new).translate(rna_strand)
+  end
+end
+
+class NucleotideStrandTranslator
+  def initialize(translation)
+    @translation = translation 
+  end
+
+  def translate(source_strand)
+    translated_nucleotides_of(source_strand).join 
+  end
+
+  private
+  attr_reader :translation
+
+  def translated_nucleotides_of(strand)
+    nucleotides_within(strand).map do |nucleotide|
+      translation.translate(nucleotide) 
+    end
+  end
+
+  def nucleotides_within(strand)
+    strand.chars
+  end
+end
+
+class RnaToDnaTranslation
+  def translate(nucleotide)
+    case nucleotide
+      when "C" then "G"
+      when "G" then "C"
+      when "U" then "A"
+      when "A" then "T"
+    end
+  end
+end
+
+class DnaToRnaTranslation
+  def translate(nucleotide)
+    case nucleotide
+      when "C" then "G"
+      when "G" then "C"
+      when "T" then "A"
+      when "A" then "U"
+    end
   end
 end
